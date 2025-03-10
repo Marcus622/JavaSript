a4931105e4640e7ad3743a71a648468c1e7ec658
Marcus622 <marcus.vix@tn.techstarter.de>
2025-03-10 21:02:01 +0100

    Update test.js

--- test.js
+++ test.js
@@ -42,3 +42,29 @@
 // function toCase(text) {
 //         return text.toLowerCase() +'-' + text.toUpperCase();
 // }
+
+// function createHTMLlist(liste) {
+//     let htmlElements = liste.map((listItem) => `<li>${listItem}</li>`);
+    
+//     let flattenedList = htmlElements.join(""); // Kein Komma zwischen den Einträgen
+    
+//     let ergebnis = `<ul>${flattenedList}</ul>`; // Richtige Template-Syntax
+    
+//     return ergebnis;
+// }
+
+// let my_list = ["lukas", "tom", "sabine", "alf"];
+// console.log(createHTMLlist(my_list));
+
+function createHTMLList(liste){
+
+    let htmlElements = liste.map((listItem) => `<li>${listItem}</li>`)
+    console.log(htmlElements)
+    let flattenedList = htmlElements.join("")
+    console.log(flattenedList)
+    let ergebnis = `<ul>${flattenedList}</ul>`
+    return ergebnis
+}
+let my_list = ["lukas", "tom", "sabine", "alf"];
+console.log(createHTMLList(my_list));
+document.body.innerHTML = createHTMLList(my_list);
